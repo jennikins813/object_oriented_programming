9456ee998cb00e205e0b27edc098f8d45fbee55c
jennikins813 <jennikins813@yahoo.com>
2014-03-27 15:31:07 -0400

    completed exercise 1

--- people.rb
+++ people.rb
@@ -1,17 +1,29 @@
-class Student	#create class Student
-	def learn	#method learn which returns quote
+class Person
+	def initialize(name)
+		@name = name
+		puts "Hi, my name is #{name}!"
+	end
+end
+
+class Student < Person
+	def learn
 		puts "I get it!"
 	end
 end
 
-s = Student.new
-s.learn
-
-class Instructor	#create class Instructor
-	def teach		#method learn which returns quote
+class Instructor < Person
+	def teach
 		puts "Everything in Ruby is an Object."
 	end
 end
 
-i = Instructor.new
-i.teach
+s = Student.new("Jen")		#Hi, my name is Jen!
+s.learn						#I get it!
+
+i = Instructor.new("Chris")		#Hi, my name is Chris!
+i.teach							#Everything in Ruby is an Object.
+
+s.teach		#error:undefined method teach for Student.
+			#cannot call the teach method on student bcoz the teach method
+			#is only defined in class Instructor. A method in a class only
+			#exists in that class.
