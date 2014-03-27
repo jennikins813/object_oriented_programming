35f44b1e0725f19ef68c49c6d0b4d31125bfe0e8
jennikins813 <jennikins813@yahoo.com>
2014-03-27 13:38:00 -0400

    created parent class Person with attr name

--- people.rb
+++ people.rb
@@ -1,14 +1,24 @@
-class Student
+class Person  # create parent class Person
+	def initialize(name)
+		@name = name
+	end
+
+	def display
+		puts "Hello my name is #{name}."
+	end
+end
+n = Person.new
+
+class Student  # create class Student with method learn
 	def learn
 		puts "I get it!"
 	end
 end
 
-class Instructor
+class Instructor # create class Instructor with method teach
 	def teach
 		puts "Everything in Ruby is an Object."
 	end
 end
 
-puts "hello"
 
